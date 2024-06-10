5d07e05931a25382be56ab4657be6ff601170390
Sebas4120 <sebastianaguirrem49@gmail.com>
2024-06-10 19:27:05 -0400

    Adding Employee Model

---
+++ WorkPortal_ASP.Net/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkPortal_ASP.Net.Models
+{
+    public class Employee
+    {   
+        //PK
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        // FK
+        [Required]
+        public int DepartmentId { get; set; }
+    }
+}
